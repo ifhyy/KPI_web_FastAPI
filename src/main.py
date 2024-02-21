d42b0f4afc27d556fc82501216e20818d069da63
stupk <bozzya7@gmail.com>
2024-02-21 13:01:17 +0100

    feat: models + migrations

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from . import models
+from .database import engine, SessionLocal
+
+
+models.Model.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ src/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timezone
+from typing import List
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
+from src.database import Model
+import email_validator
+
+
+class Visit(Model):
+    __tablename__ = 'visits'
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    date: Mapped[datetime]
+
+    doctor: Mapped["Doctor"] = relationship(back_populates="visits")
+    patient: Mapped["User"] = relationship(back_populates="visits")
+
+
+class Doctor(Model):
+    __tablename__ = 'doctors'
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    full_name: Mapped[str] = mapped_column(String(64))
+
+    visits: Mapped[List[Visit] | None] = relationship(back_populates='doctor')
+
+
+class User(Model):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    is_admin: Mapped[bool] = mapped_column(default=False)
+    email: Mapped[str] = mapped_column()
+    password_hash: Mapped[str] = mapped_column(String(256))
+    registration_date: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc))
+
+    visits: Mapped[List[Visit] | None] = relationship(back_populates='patient')
+
+    @validates('email')
+    def validate_email(self, key, email):
+        return email_validator.validate_email(email)
